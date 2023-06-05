aac10b756ae75fdef0eacf2c339379ca340a3d7f
Bakulina Daria <dbakulina0404@gmail.com>
2023-06-05 14:33:44 +0300

    adding a burger menu

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     infinite: true,
     slidesToScroll: 1,
     speed: 1000,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 2000,
 
   });
@@ -46,3 +46,44 @@ if ($(window).width() >= 992) {
   });
 };
 
+// document.addEventListener("click", function(event) {
+//   const target = event.target;
+//   if (!target.closest(".nav__item")) {
+//     const dropdownMenus = document.querySelectorAll(".dropdown-menu");
+//     dropdownMenus.forEach(function(menu) {
+//       menu.style.display = "none";
+//     });
+//   }
+// });
+
+
+const burgerMenu = document.querySelector('.burger-menu');
+const menuButton = document.querySelector('.burger-menu__button');
+const closeButton = document.querySelector('.burger-menu__close');
+
+menuButton.addEventListener('click', function() {
+  burgerMenu.classList.add('open');
+  menuButton.style.display = 'none';
+  closeButton.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function() {
+  burgerMenu.classList.remove('open');
+  menuButton.style.display = 'block';
+  closeButton.style.display = 'none';
+});
+
+// код для закриття меню при натисканні на інше місце на сторінці
+document.addEventListener('click', function(event) {
+  const target = event.target;
+  const isMenuOpen = burgerMenu.classList.contains('open');
+  const isMenuButton = target.classList.contains('burger-menu__button');
+  const isCloseButton = target.classList.contains('burger-menu__close');
+  const isMenu = target.classList.contains('burger-menu');
+  
+  if (isMenuOpen && !isMenuButton && !isCloseButton && !isMenu) {
+    burgerMenu.classList.remove('open');
+    menuButton.style.display = 'block';
+    closeButton.style.display = 'none';
+  }
+});
